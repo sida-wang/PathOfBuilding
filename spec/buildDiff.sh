18623c5023b2a739ec92269d641667914ee57a75
Paliak <91493239+Paliak@users.noreply.github.com>
2024-01-06 11:08:58 +0100

    FIX: temp batch files getting clobbered

--- spec/buildDiff.sh
+++ spec/buildDiff.sh
@@ -14,14 +14,14 @@ then
     cp -rf --parents $WORKDIR /tmp && cd /tmp$WORKDIR && \
     git restore . && git clean -fd && git checkout origin/dev && \
     cp $WORKDIR/.busted /tmp$WORKDIR/.busted && cp $WORKDIR/src/HeadlessWrapper.lua /tmp$WORKDIR/src/HeadlessWrapper.lua && rm -rf /tmp$WORKDIR/spec/ && cp -r $WORKDIR/spec/ /tmp$WORKDIR/spec/ && \
-    cat $WORKDIR/spec/builds.txt | parallel --will-cite --ungroup --pipe -N50 "cat > /tmp/parallel_links_{#};  BUILDLINKS=/tmp/parallel_links_{#} BUILDCACHEPREFIX=${CACHEDIR} busted --lua=luajit -r generate" && \
+    cat $WORKDIR/spec/builds.txt | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS=${LINKSBATCH} BUILDCACHEPREFIX=${CACHEDIR} busted --lua=luajit -r generate' && \
     BUILDCACHEPREFIX=${CACHEDIR} busted --lua=luajit -r generate && date > "$CACHEDIR/$devref" && echo "[+] Build cache computed for $devref" && cd $WORKDIR
 fi
 
 if [[ -f "$CACHEDIR/$devref" ]] #Make sure cache of dev branch builds exists and matches current dev
 then
     rm -rf /tmp/*
-    cat $WORKDIR/spec/builds.txt | parallel --will-cite --ungroup --pipe -N50 'cat > /tmp/parallel_links_{#};  BUILDLINKS="/tmp/parallel_links_{#}" BUILDCACHEPREFIX="/tmp" busted --lua=luajit -r generate' && \
+    cat $WORKDIR/spec/builds.txt | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS=${LINKSBATCH} BUILDCACHEPREFIX="/tmp" busted --lua=luajit -r generate' && \
     BUILDCACHEPREFIX='/tmp' busted --lua=luajit -r generate && date > "/tmp/$headref" && echo "[+] Build cache computed for $headref"
 fi
 
